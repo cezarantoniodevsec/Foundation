d0eeaeaf2e6a080a26b977d90bb8695a9cadbdf3
cezar <cezarantoniodev@gmail.com>
2023-10-06 22:34:08 -0300

    QuickSort Insertion

---
+++ TestsCSharpFoundation/ALGORITHMS/UTQuickSort.cs
@@ -0,0 +1,73 @@
+using CSharpFoundation.ALGORITHMS;
+
+namespace TestsCSharpFoundation.ALGORITHMS
+{
+    public class UTQuickSort
+    {
+        private ISortingAlgorithm sortingAlgorithm;
+        
+        public UTQuickSort(){
+            sortingAlgorithm = new QuickSort();
+        }
+
+        [Fact]
+        public void testDisordered()
+        {
+            // Given
+            var input = TestSortUtils.DISORDERED_ARRAY;
+            var expectedResult = TestSortUtils.ORDERED_ARRAY;            
+            // When
+            var actualResult = sortingAlgorithm.sort(input);
+            // Then            
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void testBubleSortNegative()
+        {
+            // Given
+            var input  = TestSortUtils.NEGATIVE_DISORDERED_ARRAY;
+            var expectedResult = TestSortUtils.NEGATIVE_ORDERED_ARRAY;
+            // When        
+            var actualResult = sortingAlgorithm.sort(input);
+            // Then            
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void testEmptyBubleSort()
+        {
+            // Given
+            var input  = new int[]{};
+            var expectedResult = new int[]{};
+            // When        
+            var actualResult = sortingAlgorithm.sort(input);
+            // Then            
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void testBubleSortWorstCase()
+        {
+            // Given
+            var input  = TestSortUtils.WORST_CASE;
+            var expectedResult = TestSortUtils.WORST_CASE_ORDERED;
+            // When        
+            var actualResult = sortingAlgorithm.sort(input);
+            // Then            
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void testBubleSortBestCase()
+        {
+            // Given
+            var input  = TestSortUtils.BEST_CASE;
+            var expectedResult = TestSortUtils.BEST_CASE_ORDERED;
+            // When        
+            var actualResult = sortingAlgorithm.sort(input);
+            // Then            
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
